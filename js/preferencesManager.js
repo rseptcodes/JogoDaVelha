5b60703b8d16e457013b95e4eaf3da0cf81786cb
rsept <226774053+rseptcodes@users.noreply.github.com>
2026-08-15 08:42:49 -0300

    feat: add languageManager for multilingual support

--- js/preferencesManager.js
+++ js/preferencesManager.js
@@ -1,4 +1,5 @@
 export const difficultyManager = {
+	// I'll refactor these magic numbers in the future
 	difficulty: 2, // 0: random, 1: decisionEngine, 2: miniMaxEngine
 	init(){
 		const saved = (localStorage.getItem("pref-difficulty"));
@@ -25,4 +26,44 @@ export const themeManager = {
         console.log(theme);
         localStorage.setItem('pref-theme', theme);
 	},
+};
+export const languageManager = {
+    language: "en",
+    translations: {
+        pt: {
+            drawingStarter: "Sorteando quem começa...",
+            yourTurn: "Sua vez!",
+            thinking: "Pensando...",
+            cpuEasy: "KKKKK que fácil",
+            playerCanWin: "@%$%@!",
+            playerWin: "Você venceu!",
+            cpuWin: "A CPU venceu!",
+            draw: "Empate!"
+        },
+
+        en: {
+            drawingStarter: "Choosing who goes first...",
+            yourTurn: "Your turn!",
+            thinking: "Thinking...",
+            cpuEasy: "LOL, too easy",
+            playerCanWin: "@%$%@!",
+            playerWin: "You won!",
+            cpuWin: "CPU won!",
+            draw: "Draw!"
+        }
+    },
+
+    init() {
+        const browserLanguage = navigator.language.toLowerCase();
+
+        this.language = browserLanguage.startsWith("pt")
+            ? "pt"
+            : "en";
+    },
+
+    getText(key) {
+    return this.translations[this.language]?.[key]
+        ?? this.translations.en[key]
+        ?? key;
+},
 };
