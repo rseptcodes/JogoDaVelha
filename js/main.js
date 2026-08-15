3146646dbd7cb26ce3b546375e9dfef02e2f6eab
rsept <226774053+rseptcodes@users.noreply.github.com>
2026-08-15 08:41:14 -0300

    feat: add languageManager support to ui.updateStatus
    - Replace hardcoded status messages with languageManager translations
    - Add English language support
    - Centralize status message translations
    - Remove the previous hardcoded approach to make future languages easier to add
    - Add a fallback to English when a translation is unavailable

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import { gameRules } from './gameRules.js';
 import { turnManager } from './turnManager.js';
 import { decisionEngine } from './aiEngine.js';
 import { difficultyManager } from './preferencesManager.js';
-import { themeManager } from './preferencesManager.js';
+import { themeManager, languageManager } from './preferencesManager.js';
 import { scoreManager } from './scoreManager.js';
 import { helperFunctions } from './helperFunctions.js';
 
@@ -25,7 +25,8 @@ const mainConfig = {
         // init subs
         eventBus.subscribe(() => {
             cellManager.initBoard();
-            ui.updateStatus("Sorteando quem começa...");
+					  languageManager.init();
+            ui.updateStatus(languageManager.getText("drawingStarter"));
             turnManager.startGame();
             ui.setBodyGradient();
         }, "init");
@@ -44,41 +45,40 @@ const mainConfig = {
     
         // turn subs
         eventBus.subscribe(() => {
-            ui.updateStatus("Sua vez!");
-            helperFunctions.manageClick(false, boardState.boardElement);
+					ui.updateStatus(languageManager.getText("yourTurn"));            helperFunctions.manageClick(false, boardState.boardElement);
         }, "playerTurn");
     
         eventBus.subscribe(() => {
-            ui.updateStatus("Pensando...");
+        ui.updateStatus(languageManager.getText("thinking"));
             helperFunctions.manageClick(true, boardState.boardElement);
             decisionEngine.makeDecision(difficultyManager.difficulty);
         }, "cpuTurn");
         
         // canWins
         eventBus.subscribe(() => {
-            ui.updateStatus("KKKKK que facil");
+            ui.updateStatus(languageManager.getText("cpuEasy"));
         }, "cpuCanWin");
         eventBus.subscribe(() => {
-            ui.updateStatus("@%$%@!");
+            ui.updateStatus(languageManager.getText("playerCanWin"));
         }, "playerCanWin");
         
         // playerWin subs
         eventBus.subscribe(() => {
-            ui.updateStatus("Você venceu!");
+            ui.updateStatus(languageManager.getText("playerWin"));
             scoreManager.incrementScore("player");
             ui.setBodyGradient("playerVictory");
         }, "playerWin");
           
         // cpuWin subs
         eventBus.subscribe(() => {
-            ui.updateStatus("A CPU venceu!");
+            ui.updateStatus(languageManager.getText("cpuWin"));
             ui.setBodyGradient("CPUVictory");
             scoreManager.incrementScore("cpu");
         }, "cpuWin");
           
         // tie subs
         eventBus.subscribe(() => {
-            ui.updateStatus("Empate!");
+            ui.updateStatus(languageManager.getText("draw"));
             ui.setBodyGradient("draw");
             scoreManager.incrementScore("tie");
         }, "tie");
